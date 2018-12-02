d80770bbca26f4d4bdeb6c3b48a24f1154686c75
sadeen20-meet <sadeen20@meet.mit.edu>
2018-12-02 20:03:44 +0200

    lab7

--- lab7.py
+++ lab7.py
@@ -1,18 +1,33 @@
-from turtle import *
-import random
 import turtle
+from turtle import Turtle
 import math
 class Ball(Turtle):
-	def __init__(self, radius, color, speed):
+	def __init__(self, x, y, radius, dx, dy, color):
 		Turtle.__init__(self)
 		self.shape("circle")
-		self.shapesize(radius/10)
+		self.goto(x,y)
 		self.radius=radius
+		self.dx=dx
+		self.dy=dy
+		self.shapesize(radius/10)
 		self.color(color)
-		self.speed(speed)
-ball1=Ball(50,"pink",0)		
-ball2=Ball(40,"blue",0)
-def check_collision(ball1, ball2):
+	def move(self,width,height):
+		self.width=width
+		self.height=height
+		oldx=self.xcor()
+		oldy=self.ycor()
+		newx=oldx+self.dx
+		newy=oldy+self.dy
+		Ball.goto(newx,newy)
+
+
+
+
+		
+ball1=Ball(100,100,20, 5, 5, "pink")		
+ball2=Ball(50,50,20,3,3,"blue")
+	def check_collision(ball1, ball2):
 
 	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+	if(D >)
 turtle.mainloop()
