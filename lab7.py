9a3ed4abba364b9295ff614d4c29e20c0d23de89
sadeen20-meet <sadeen20@meet.mit.edu>
2018-12-16 19:54:30 +0200

    lab7

--- lab7.py
+++ lab7.py
@@ -1,33 +1,53 @@
 import turtle
 from turtle import Turtle
 import math
+import random
+turtle.tracer(0)
 class Ball(Turtle):
-	def __init__(self, x, y, radius, dx, dy, color):
+	def __init__(self, radius, dx, dy, color):
 		Turtle.__init__(self)
 		self.shape("circle")
-		self.goto(x,y)
 		self.radius=radius
-		self.dx=dx
-		self.dy=dy
+		self.dx=random.randint(20,40)/40
+		self.dy=random.randint(30,50)/40
 		self.shapesize(radius/10)
 		self.color(color)
 	def move(self,width,height):
-		self.width=width
-		self.height=height
 		oldx=self.xcor()
 		oldy=self.ycor()
 		newx=oldx+self.dx
 		newy=oldy+self.dy
-		Ball.goto(newx,newy)
+		if newx >= width or newx <= -width:
+			self.dx= -self.dx
+			newx=oldx + self.dx
+		if newy>= height or newy<= -height:
+			self.dy= -self.dy
+			newy=oldy + self.dy
+		self.goto(newx,newy)
 
 
+ball2=Ball(20,3,3,"blue")		
+ball1=Ball(20, 5, 5, "pink")
 
-
-		
-ball1=Ball(100,100,20, 5, 5, "pink")		
-ball2=Ball(50,50,20,3,3,"blue")
-	def check_collision(ball1, ball2):
-
+def check_collision(ball1, ball2):
+	x2=ball1.xcor()
+	x1=ball2.ycor()
+	y2=ball1.xcor()
+	y1=ball2.ycor()
 	D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
-	if(D >)
-turtle.mainloop()
+	if ball1.radius + ball2.radius >= D:
+		return True
+	else:
+		return False
+
+	
+while True:
+	ball1.penup()
+	ball1.move(300,300)	
+	ball2.penup()
+	ball2.move(300,300)
+	if check_collision(ball1,ball2):
+		print("we have a collision")
+	turtle.update()
+
+	
