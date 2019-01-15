40d6757032c0bf59a891917901048f8ffec8dc20
sadeen20-meet <sadeen20@meet.mit.edu>
2019-01-15 20:01:43 +0200

    individual project

--- individualptoject.py
+++ individualptoject.py
@@ -3,11 +3,11 @@ from turtle import Turtle
 import time
 import math
 import random 
-turtle.tracer(1)
+turtle.tracer(0,0)
 turtle.penup()
 turtle.hideturtle()
 score=0
-pos_list=[]
+
 UP_ARROW = "Up" 
 LEFT_ARROW = "Left"
 DOWN_ARROW = "Down"
@@ -48,7 +48,7 @@ def right():
     if not direction == LEFT:
         direction=RIGHT
    
-        print('youpressed the right key!')
+        print('you pressed the right key!')
 
 
 turtle.onkeypress(up, UP_ARROW) 
@@ -62,16 +62,57 @@ class Ball(Turtle):
 	def __init__(self, radius, x, y, dx, dy, color):
 		Turtle.__init__(self)
 		self.shape("circle")
+		self.penup()
 		self.radius=radius
 		self.x=x
 		self.y=y
-		self.dx=random.randint(20,40)/40
-		self.dy=random.randint(30,50)/40
+		self.dx=random.randint(120,150)/40
+		self.dy=random.randint(120,150)/40
 		self.shapesize(radius/10)
 		self.color(color)
-	def move_Ball (self,screen_width,screen_height):
-		
+		self.goto(x, y)
 
+	def move_my_ball(self):
+		if direction==RIGHT:
+			self.goto( self.xcor() + self.dx,self.ycor())
+			print("You moved right!")
+		
+		elif direction==LEFT:
+			self.goto(self.xcor() - self.dx,self.ycor())
+			print("You moved left!")
+		
+		elif direction==UP:
+			self.goto(self.xcor(), self.ycor()+self.dy)
+			print('you moved up !')
+			
+		elif direction==DOWN:
+			self.goto(self.xcor(), self.ycor()-self.dy)
+			print('you moved down!')
+
+	def move(self, width, height):
+		oldx=self.xcor()
+		oldy=self.ycor()
+		newx=oldx+self.dx
+		newy=oldy+self.dy
+		if newx >= width or newx <= -width:
+			self.dx= -self.dx
+			newx=oldx + self.dx
+		if newy>= height or newy<= -height:
+			self.dy= -self.dy
+			newy=oldy + self.dy
+		self.goto(newx,newy)
+		turtle.penup()
+
+
+	
+
+def move_all_balls(BALLS, width,height):
+	for BALL in BALLS:
+		BALL.move(width, height)
+		
+			
+
+		
 
 
 RUNNING=True 
@@ -88,9 +129,9 @@ MAXIMUM_BALL_DX=5
 MINIMUM_BALL_DY=-5
 MAXIMUM_BALL_DY=5
 BALLS=[]
-for BALL in range (NUMBER_OF_BALLS):
+for i in range (NUMBER_OF_BALLS):
 	RADUIS=random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
-	x=random.randint(-SCREEN_WIDTH+MAXIMUM_BALL_RADIUS,SCREEN_HEIGHt- MAXIMUM_BALL_RADIUS)
+	x=random.randint(-SCREEN_WIDTH+MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT- MAXIMUM_BALL_RADIUS)
 	y=random.randint(-SCREEN_HEIGHT+MAXIMUM_BALL_RADIUS,SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
 	dx=random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
 	dy=random.randint(MINIMUM_BALL_DY,MAXIMUM_BALL_DY)
@@ -98,31 +139,15 @@ for BALL in range (NUMBER_OF_BALLS):
 	BALL=Ball(RADUIS, x, y, dx, dy, color)
 	BALLS.append(BALL)
 
-#moving code!!!
-	def move(self,width,height):
-		oldx=self.xcor()
-		oldy=self.ycor()
-		newx=oldx+self.dx
-		newy=oldy+self.dy
-		if newx >= width or newx <= -width:
-			self.dx= -self.dx
-			newx=oldx + self.dx
-		if newy>= height or newy<= -height:
-			self.dy= -self.dy
-			newy=oldy + self.dy
-		self.goto(newx,newy)
-
-
-
 border=turtle.clone()
 border.ht()
 border.penup()
-border.goto(-300,300)
+border.goto(-500,500)
 border.pendown()
-border.goto(300,300)
-border.goto(300,-300)
-border.goto(-300,-300)
-border.goto(-300,300)
+border.goto(500,500)
+border.goto(500,-500)
+border.goto(-500,-500)
+border.goto(-500,500)
 
 #numbers label!
 num_label=turtle.Turtle()
@@ -130,5 +155,17 @@ num_label.ht()
 num_label.penup()
 num_label.color('blue')
 num_label.width('10')
-num_label.goto(0,-400)
-num_label.write(str (score))
+num_label.goto(600,-550)
+num_label.write(str(score))
+
+while True:
+	MY_BALL.penup()
+	MY_BALL.move_my_ball()
+	turtle.update()
+	move_all_balls(BALLS, 500, 500)
+	turtle.update()
+	time.sleep(SLEEP)
+
+
+
+turtle.mainloop()
